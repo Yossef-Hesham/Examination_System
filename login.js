a7f0278bb6d32f33ce8592d4328c6eac5940a16c
Youssef-Hesham <youss4923@gmail.com>
2025-12-30 19:52:34 +0200

    merged

---
+++ login.js
@@ -0,0 +1,44 @@
+var login_button = document.getElementById("login_button");
+
+function validate_login() {
+    var email = document.getElementById('login_e').value;
+    var password = document.getElementById('login_p').value;
+
+    // Reset errors
+    document.getElementById('emailError').innerText = '';
+    document.getElementById('passwordError').innerText = '';
+
+    var flag = true;
+
+    if (email == '') {
+        document.getElementById('emailError').innerText = 'Email is required';
+        flag = false;
+    }
+
+    if (password == '') {
+        document.getElementById('passwordError').innerText = 'Password is required';
+        flag = false;
+    }
+
+    if (flag) {
+
+        if(window.localStorage['email'] === email &&
+            window.localStorage['password'] === password)
+            
+            console.log("log in successful!")
+        else 
+            document.getElementById('log_failed').innerText = 'email or password is wrong, please try again';
+            // alert("email or password is wrong, please try again");
+    }
+}
+
+
+
+ login_button.addEventListener("click", function (e) {
+        e.preventDefault(); 
+        validate_login();
+
+
+
+
+});
